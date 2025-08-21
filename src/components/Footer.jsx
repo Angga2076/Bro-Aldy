565e10d166bcab15d95c44b0bfcfe48f4fea69f7
Angga2076 <rismanksayang@gmail.com>
2025-08-21 20:25:51 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,12 @@
-
+export default function Footer() {
+return (
+<footer className="bg-gray-800 p-6 text-center mt-10">
+<p>© 2025 Your Name. All rights reserved.</p>
+<div className="flex justify-center gap-6 mt-4">
+<a href="#">GitHub</a>
+<a href="#">Instagram</a>
+<a href="#">YouTube</a>
+</div>
+</footer>
+);
+}
